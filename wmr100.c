ab1e09c58cf23ddf65bcd8bfbdc3754604bb04d9
Maxim Penzin <maxp@ya.ru>
2014-05-03 21:51:27 +0900

    refactor data handling

--- wmr100.c
+++ wmr100.c
@@ -23,12 +23,10 @@
 
 #include <hid.h>
 #include <stdio.h>
-#include <string.h>
+// #include <string.h>
 #include <signal.h>
-/* #include <time.h> */
 #include <unistd.h>
 #include <ctype.h>
-/* #include <sys/time.h> */
 
 #define WMR100_VENDOR_ID  0x0fde
 #define WMR100_PRODUCT_ID 0xca01
@@ -53,30 +51,6 @@ typedef struct _WMR {
     HIDInterface *hid;
 } WMR;
 
-/*
-void dump_packet(unsigned char *packet, int len)
-{
-    int i;
-    printf("Receive packet len %d: ", len);
-    for(i = 0; i < len; ++i) {
-        printf("%02x ", (int)packet[i]);
-    }
-    printf("\n");
-}
-
-
-WMR *wmr_new()
-{
-    WMR *wmr = malloc(sizeof(WMR));
-    wmr->remain = 0;
-    wmr->buffer = malloc(BUF_SIZE);
-    if (wmr->buffer == NULL) {
-      free(wmr);
-      return NULL;
-    }
-    return wmr;
-}
-*/
 
 void wmr_send_packet_init(WMR *wmr)
 {
@@ -106,12 +80,6 @@ int wmr_init(WMR *wmr)
     HIDInterfaceMatcher matcher = { WMR100_VENDOR_ID, WMR100_PRODUCT_ID, NULL, NULL, 0 };
     int retries;
 
-    /* see include/debug.h for possible values */
-    /*hid_set_debug(HID_DEBUG_ALL);*/
-    /*hid_set_debug_stream(stderr);*/
-    /* passed directly to libusb */
-    /*hid_set_usb_debug(0);*/
-  
     ret = hid_init();
     if (ret != HID_RET_SUCCESS) {
         fprintf(stderr, "hid_init failed with return code %d\n", ret);
@@ -200,11 +168,8 @@ void wmr_read_packet(WMR *wmr)
 
 unsigned char wmr_read_byte(WMR *wmr)
 {
-    while(wmr->remain == 0) {
-        wmr_read_packet(wmr);
-    }
+    while(wmr->remain == 0) { wmr_read_packet(wmr); }
     wmr->remain--;
-
     return wmr->buffer[wmr->pos++];
 }
 
@@ -224,235 +189,46 @@ int verify_checksum(unsigned char * buf, int len)
 }
 
 /*
-void wmr_log_data(WMR *wmr, char *topic, char *msg) 
+void wmr_handle_rain(WMR *wmr, unsigned char *data, int len) 
 {
-    // char timestamp[200];
-    char *buf;
-    // struct tm *tmp;
-    // struct timeval tv;
+    int sensor, power, rate;
+    float hour, day, total;
+    int smi, sho, sda, smo, syr;
+    char *msg;
+    
+    sensor = data[2] & 0x0f;
+    power = data[2] >> 4;
+    rate = data[3];
+    
+    hour = ((data[5] << 8) + data[4]) * 25.4 / 100.0;  // mm
+    day = ((data[7] << 8) + data[6]) * 25.4 / 100.0;   // mm
+    total = ((data[9] << 8) + data[8]) * 25.4 / 100.0; // mm
 
-    // gettimeofday(&tv, NULL);
-    // tmp = gmtime(&tv.tv_sec);
+    smi = data[10];
+    sho = data[11];
+    sda = data[12];
+    smo = data[13];
+    syr = data[14] + 2000;
 
-    // strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tmp);
-    asprintf(&buf,
-        "{\"topic\": \"%s\", "
-        // "\"timestamp\": \"%s.%06d\", "
-        "%s}",
-        topic,
-        // timestamp,
-        // (int)tv.tv_usec,
-        msg);
-
-    printf("%s\n", buf);
-    free(buf);
+    // print
 }
 */
 
-/****************************
-  Data handlers
- ****************************/
-
-void wmr_handle_rain(WMR *wmr, unsigned char *data, int len) 
+void print_temp(unsigned char *data)
 {
-    // NOTE: function disabled
-
-    // int sensor, power, rate;
-    // float hour, day, total;
-    // int smi, sho, sda, smo, syr;
-    // char *msg;
-    
-    // sensor = data[2] & 0x0f;
-    // power = data[2] >> 4;
-    // rate = data[3];
-    
-    // hour = ((data[5] << 8) + data[4]) * 25.4 / 100.0; /* mm */
-    // day = ((data[7] << 8) + data[6]) * 25.4 / 100.0; /* mm */
-    // total = ((data[9] << 8) + data[8]) * 25.4 / 100.0; /* mm */
-
-    // smi = data[10];
-    // sho = data[11];
-    // sda = data[12];
-    // smo = data[13];
-    // syr = data[14] + 2000;
-
-    // asprintf(&msg,
-    //          "\"sensor\": %d, "
-    //          "\"power\": %d, "
-    //          "\"rate\": %d, "
-    //          "\"hour_total\": %.2f, "
-    //          "\"day_total\": %.2f, "
-    //          "\"all_total\": %.2f, "
-    //          "\"since\": \"%04d%02d%02d%02d%02d\", "
-    //          "\"source\": \"wmr100.%d\", "
-    //          "\"origin\": \"wmr100\"",
-    //          sensor, power, rate, hour, day, total, syr, smo, sda, sho, smi, sensor);
-    // wmr_log_data(wmr, "rain", msg);
-    // free(msg);
-}
-
-void wmr_handle_temp(WMR *wmr, unsigned char *data, int len)
-{
-    int sensor, humidity; // st, smiley, trend,
     float temp, dewpoint;
-    // // char *msg;
-
-    sensor = data[2] & 0x0f;
-    // st = data[2] >> 4;
-    // smiley = st >> 2;
-    // trend = st & 0x03;
-    // trend -= 1;
 
     temp = (data[3] + ((data[4] & 0x0f) << 8)) / 10.0;
-    if ((data[4] >> 4) == 0x8) { temp = -temp; }
-    
-    humidity = data[5];
+    if(data[4] & 0x80) { temp = -temp; }
 
     dewpoint = (data[6] + ((data[7] & 0x0f) << 8)) / 10.0;
-    if ((data[7] >> 4) == 0x8) { dewpoint = -dewpoint; }
+    if(data[7] & 0x80) { dewpoint = -dewpoint; }
 
-    printf("* s=%d t=%.1f h=%d d=%.1f\n", sensor, temp, humidity, dewpoint);
-
-    // asprintf(&msg,
-    //          "\"sensor\": %d, "
-    //          "\"smile\": %d, "
-    //          "\"trend\": %d, "
-    //          "\"temp\": %.1f, "
-    //          "\"humidity\": %d, "
-    //          "\"dewpoint\": %.1f, "
-    //          "\"source\": \"wmr100.%d\", "
-    //          "\"origin\": \"wmr100\"",
-    //          sensor, smiley, trend, temp, humidity, dewpoint, sensor);
-    // wmr_log_data(wmr, "temp", msg);
-    // free(msg);
+    printf("* s=%d t=%.1f h=%d d=%.1f pwr=%d\n", 
+        data[2] & 0x0f, temp, data[5], dewpoint, data[0] >> 6
+    );
 }
 
-void wmr_handle_water(WMR *wmr, unsigned char *data, int len)
-{
-    /*
-    int sensor;
-    float temp;
-    char *msg;
-
-    sensor = data[2] & 0x0f;
-
-    temp = (data[3] + ((data[4] & 0x0f) << 8)) / 10.0;
-    if ((data[4] >> 4) == 0x8)
-        temp = -temp;
-
-    asprintf(&msg,
-             "\"sensor\": %d, "
-             "\"temp\": %.1f, "
-             "\"source\": \"wmr100\", "
-             "\"origin\": \"wmr100\"",
-             sensor, temp);
-    wmr_log_data(wmr, "water", msg);
-    free(msg);
-    */
-}
-
-void wmr_handle_pressure(WMR *wmr, unsigned char *data, int len)
-{
-    printf("* p=%d\n", data[2] + ((data[3] & 0x0f) << 8));
-
-    /*
-    int pressure, forecast, alt_pressure, alt_forecast;
-    char *msg;
-
-    pressure = data[2] + ((data[3] & 0x0f) << 8);
-    
-    forecast = data[3] >> 4;
-    alt_pressure = data[4] + ((data[5] & 0x0f) << 8);
-    alt_forecast = data[5] >> 4;
-
-    asprintf(&msg,
-             "\"pressure\": %d, "
-             "\"forecast\": %d, "
-             "\"altpressure\": %d, "
-             "\"altforecast\": %d, "
-             "\"source\": \"wmr100\", "
-             "\"origin\": \"wmr100\"",
-             pressure, forecast, alt_pressure, alt_forecast);
-    wmr_log_data(wmr, "pressure", msg);
-    free(msg);
-    */
-}
-
-void wmr_handle_uv(WMR *wmr, unsigned char *data, int len)
-{
-    // NOTE: not implemented
-
-    // char *msg;
-
-    // asprintf(&msg, "\"todo\": 1");
-    // wmr_log_data(wmr, "uv", msg);
-    // free(msg);
-}
-
-void wmr_handle_wind(WMR *wmr, unsigned char *data, int len)
-{
-    printf("* w=%.1f b=%d pwr=%02x\n", data[4] / 10.0, data[2] & 0xf, data[2] >> 4);
-
-/*
-    char *msg;
-    int wind_dir, power, low_speed, high_speed;
-    float wind_speed, avg_speed;
-
-    wind_dir = data[2] & 0xf;
-    power = data[2] >> 4;
-    
-    wind_speed = data[4] / 10.0;
-
-    low_speed = data[5] >> 4;
-    high_speed = data[6] << 4;
-    avg_speed = (high_speed + low_speed) / 10.0;
-
-    asprintf(&msg,
-             "\"power\": %d, "
-             "\"dir\": %d, "
-             "\"speed\": %.1f, "
-             "\"avgspeed\": %.1f, "
-             "\"source\": \"wmr100\", "
-             "\"origin\": \"wmr100\"",
-             power, wind_dir, wind_speed, avg_speed);
-    wmr_log_data(wmr, "wind", msg);
-    free(msg);
-*/    
-}
-
-void wmr_handle_clock(WMR *wmr, unsigned char *data, int len)
-{
-    if(data[0]) {
-        printf("* pwr=%02x\n", data[0] >> 4);
-    }
-
-    // int power, powered, battery, rf, level, mi, hr, dy, mo, yr;
-    // char *msg;
-
-    // power = data[0] >> 4;
-    // powered = power >> 3;
-    // battery = (power & 0x4) >> 2;
-    // rf = (power & 0x2) >> 1;
-    // level = power & 0x1;
-
-    // mi = data[4];
-    // hr = data[5];
-    // dy = data[6];
-    // mo = data[7];
-    // yr = data[8] + 2000;
-
-    // asprintf(&msg,
-    //          "\"at\": \"%04d%02d%02d%02d%02d\", "
-    //          "\"powered\": %d, "
-    //          "\"battery\": %d, "
-    //          "\"rf\": %d, "
-    //          "\"level\": %d, "
-    //          "\"source\": \"wmr100\", "
-    //          "\"origin\": \"wmr100\"",
-    //          yr, mo, dy, hr, mi, powered, battery, rf, level);
-    // wmr_log_data(wmr, "clock", msg);
-    // free(msg);
-}
 
 unsigned char _buffer[20];
 
@@ -486,15 +262,43 @@ void wmr_read_data(WMR *wmr)
         _buffer[1] = type;
         for (i = 2; i < data_len; ++i) { _buffer[i] = wmr_read_byte(wmr); }
 
-        if (verify_checksum(_buffer, data_len) == 0) {
-            switch(type) {
-                case 0x41: wmr_handle_rain(wmr, _buffer, data_len);     break;
-                case 0x42: wmr_handle_temp(wmr, _buffer, data_len);     break;
-                case 0x44: wmr_handle_water(wmr, _buffer, data_len);    break;
-                case 0x46: wmr_handle_pressure(wmr, _buffer, data_len); break;
-                case 0x47: wmr_handle_uv(wmr, _buffer, data_len);       break;
-                case 0x48: wmr_handle_wind(wmr, _buffer, data_len);     break;
-                case 0x60: wmr_handle_clock(wmr, _buffer, data_len);    break;
+        if (verify_checksum(_buffer, data_len) == 0) 
+        {
+            switch(type) 
+            {
+                case 0x41: 
+                    // rain: not implemented
+                    break;
+
+                case 0x42: 
+                    print_temp(_buffer);     
+                    break;
+
+                // case 0x44: break; ?water
+
+                case 0x46: 
+                    // pressure
+                    printf("* p=%d\n", data[2] + ((data[3] & 0x0f) << 8));
+                    break;
+
+                case 0x47: 
+                    // UVN800 sensor
+                    printf("* uv=%d\n", data[3]);
+                    break;
+
+                case 0x48:
+                    // wind
+                    printf("* w=%.1f b=%d pwr=%d\n", 
+                        ((data[5] & 0x0f)*256 + data[4]) / 10.0, 
+                        (data[2] & 0x0f)*360/16, 
+                        (data[0] >> 4)
+                    );
+                    break;
+
+                case 0x60: 
+                    // clock
+                    printf("* pwr=%d rf=%d\n", data[0] >> 6, (data[0] >> 4) & 0x3);            
+                    break;
             }    
         }
     }
@@ -524,13 +328,9 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    // printf("Found on USB: %s\n", wmr->hid->id);
-
     while(true) {
         wmr_read_data(wmr);
     }
- 
-    return 0;
 }
 
 //.
