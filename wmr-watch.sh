cbe780fab02dc10f2198ca1fa9056757659f557f
Maxim Penzin <maxp@ya.ru>
2014-05-07 20:07:36 +0900

    watch fix

--- wmr-watch.sh
+++ wmr-watch.sh
@@ -4,10 +4,10 @@
 # 1  *  *  *  *  root  /home/pi/wmr100-pi/wmr-watch.sh
 
 WATCH_FILE="/tmp/wmr100.cycle"
-SLEEP_TIME=2000
+SLEEP_TIME="30m"
 
 c0=`cat $WATCH_FILE`
-sleep($SLEEP_TIME)
+sleep $SLEEP_TIME
 c1=`cat $WATCH_FILE`
 
 if [ "$c0" == "$c1" ]; then 
